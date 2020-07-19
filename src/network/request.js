c4a0f6d207ad883e196d8ddb47d1cc5ca3487268
hqchqc <734986933@qq.com>
2020-07-19 15:49:43 +0800

    s

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config){
     const instance = axios.create({
         // 接口请与老师联系 
-        baseURL: 'http://127.0.0.1'
+        baseURL: 'http://123.207.32.32:8000/api/m3'
     });
 
     return instance(config);
