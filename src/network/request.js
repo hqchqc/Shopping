564753ef38aac98c7355477d7e8573b468c592e6
hqchqc <734986933@qq.com>
2020-08-01 15:55:51 +0800

    network

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 export function request(config){
     const instance = axios.create({
         // 接口请与老师联系 
-        baseURL: 'http://123.207.32.32:8000/api/m3'
+        baseURL: 'http://152.136.185.210:8000/api/z8',
+        timeout: 5000
     });
 
     return instance(config);
